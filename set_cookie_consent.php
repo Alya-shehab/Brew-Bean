21ebf9e0606804cafc91a2cdab201e87340d33eb
Alya <shehabalya@gmail.com>
2026-05-22 15:02:32 +0300

    Delete set_cookie_consent.php

--- set_cookie_consent.php
+++
@@ -1,9 +0,0 @@
-<?php
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $choice = $_POST['choice'] ?? '';
-
-    if ($choice === 'accepted') {
-        setcookie('cookie_consent', 'accepted', time() + (30 * 24 * 60 * 60), '/');
-    }
-}
-?>
